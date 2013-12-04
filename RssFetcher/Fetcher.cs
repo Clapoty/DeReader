dad936f0da914dc098059d022f4f45fb0e14ae1c
clapoty <clapoty@gmail.com>
2013-12-04 14:48:14 +0100

    Began to start using Rx for pushing rss feeds

--- RssFetcher/Fetcher.cs
+++ RssFetcher/Fetcher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.ServiceModel.Syndication;
 using System.Xml;
 using System.Xml.Linq;
@@ -12,9 +13,11 @@ namespace RssFetcher
     {
         public Dictionary<string, Article> Fetch(string uri)
         {
-            var xmlReader = XmlReader.Create(uri);
-            SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
-            xmlReader.Close();
+            SyndicationFeed feed;
+            using (var reader = XmlReader.Create(uri))
+            {
+                feed = SyndicationFeed.Load(reader);
+            }
 
             if (feed == null)
             {
@@ -22,32 +25,44 @@ namespace RssFetcher
             }
 
             var articles = new Dictionary<string, Article>();
-
-            foreach (var item in feed.Items)
+            
+            IObservable<SyndicationItem> obs = feed.Items.ToObservable();
+            using (obs.Subscribe(x => AddItemTo(GetUriArticlePair(uri, x), articles)))
             {
-                string subject = item.Title.Text;
-                string summary = item.Summary.Text;
-                List<Author> authors = new List<Author>(item.Authors.Count);
-                authors.AddRange(item.Authors.Select(author => new Author(author.Name, author.Email)));
-
-                string articleContent = "";
-                foreach (SyndicationElementExtension ext in item.ElementExtensions)
-                {
-                    if (ext.GetObject<XElement>().Name.LocalName == "encoded")
-                    {
-                        articleContent = ext.GetObject<XElement>().Value;
-                    }
-                }
-
-                var content = new Content(articleContent, "Html");
-                DateTime lastUpdateTime = item.LastUpdatedTime.DateTime;
-                DateTime publishDate = item.PublishDate.DateTime;
-
-                var article = new Article(item.Id, uri, subject, item.Title.Text, summary, authors, lastUpdateTime, publishDate, content);
-                string key = string.Format("{0}.{1}", uri, item.Id);
-                articles.Add(key, article);
+                return articles;
             }
-            return articles;
+        }
+
+        private void AddItemTo(KeyValuePair<string, Article> getUriArticlePair, Dictionary<string, Article> articles)
+        {
+            articles.Add(getUriArticlePair.Key, getUriArticlePair.Value);
+        }
+
+        private static KeyValuePair<string, Article> GetUriArticlePair(string uri, SyndicationItem item)
+        {
+            string subject = item.Title.Text;
+            string summary = item.Summary.Text;
+            var authors = new List<Author>(item.Authors.Count);
+            authors.AddRange(item.Authors.Select(author => new Author(author.Name, author.Email)));
+
+            string articleContent = "";
+            foreach (SyndicationElementExtension ext in item.ElementExtensions)
+            {
+                if (ext.GetObject<XElement>().Name.LocalName == "encoded")
+                {
+                    articleContent = ext.GetObject<XElement>().Value;
+                }
+            }
+
+            var content = new Content(articleContent, "Html");
+            DateTime lastUpdateTime = item.LastUpdatedTime.DateTime;
+            DateTime publishDate = item.PublishDate.DateTime;
+
+            var article = new Article(item.Id, uri, subject, item.Title.Text, summary, authors, lastUpdateTime, publishDate,
+                                      content);
+            var key = string.Format("{0}.{1}", uri, item.Id);
+
+            return new KeyValuePair<string, Article>(key, article);
         }
     }
 }
